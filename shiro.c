22f6b2c4aa62c2fc524bc883faf565fadd7e3d37
Bryan <bryangilbert1411@gmail.com>
2024-10-05 16:37:48 +0700

    made shiro.c, 1/4 done

--- shiro.c
+++ shiro.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 
+int findHigh(int *arr, int length);
 int main(void){
     int T;
-
     scanf("%d", &T);
+    int length;
+    for(int i = 0; i < T; i++){
+        scanf("%d", &length);
+        int arr[length];
+        for(int i = 0; i < length; i++){
+            scanf("%d", &arr[i]);
+        }
+        findHigh(arr, length);
+    }
+    return 0;
+}
+
+int findHigh(int *arr, int length){
+    int prefixSum[length];
+    for(int i = 0; i < length; i++){
+        prefixSum[i] = (i - 1 >= 0) ? prefixSum[i - 1] + arr[i] : arr[i];
+    }
+
+    printf("\n\n");
     return 0;
 }
