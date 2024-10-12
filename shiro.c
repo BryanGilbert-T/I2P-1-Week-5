3ca858af4364249b19ac44eedbbbd00e4b5deb8c
Bryan Gilbert <bryangilbert1411@gmail.com>
2024-10-12 15:08:54 +0800

    shiro 3/6

--- shiro.c
+++ shiro.c
@@ -9,21 +9,21 @@ int main(void){
 
     for(int i = 0; i < T; i++){
         scanf("%d", &N);
-        int a[N];
-        int sum;
-        int max = a[0];
+        long long a[N];
+        long long sum;
         for(int j = 0; j < N; j++){
-            scanf("%d", &a[j]);
+            scanf("%lld", &a[j]);
             a[j] = (j - 1 >= 0) ? a[j - 1] + a[j] : a[j];
         }
 
+        long long max = a[0];
         for(int i = 0; i < N; i++){
             for(int j = i; j < N; j++){
                 sum = (i - 1 >= 0) ? a[j] - a[i - 1] : a[j];
                 if(sum > max) max = sum;
-                printf("sum: %d\n", sum);
+                //printf("sum: %d\n", sum);
             }
-            printf("\nmax: %d\n\n", max);
+            //printf("\nmax: %d\n\n", max);
         }
         printf("%d\n", max);
     }
